5f906dac337cbd00c331b26597dd64c5bdb04efb
fadhdwisamns <fadhdwisam@gmail.com>
2020-08-14 17:03:08 +0700

    fadh

---
+++ app/src/main/java/com/juaracoding/fadh/MainMenu.java
@@ -0,0 +1,43 @@
+package com.juaracoding.fadh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainMenu extends AppCompatActivity {
+
+    String username, password;
+    TextView txtUsername;
+    ImageView btnTambahData;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        txtUsername = findViewById(R.id.txtUsername);
+        btnTambahData = findViewById(R.id.btnTambahData);
+        username = getIntent().getStringExtra("username");
+        password = getIntent().getStringExtra("password");
+
+
+
+        txtUsername.setText(username);
+    }
+
+    public void screenTambahData(View view){
+        Intent intent = new Intent(MainMenu.this,TambahData.class);
+        startActivity(intent);
+
+    }
+
+    public void screenListData(View view){
+        Intent intent = new Intent(MainMenu.this,ListBiodata.class);
+        startActivity(intent);
+
+    }
+
+
+}
